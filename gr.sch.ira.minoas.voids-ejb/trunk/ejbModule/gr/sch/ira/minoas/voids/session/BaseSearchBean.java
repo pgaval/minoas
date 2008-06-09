873af3fa48fbae6a0a8eb9373c7c53f33c2fc6e4
slavikos <slavikos@c46bf295-244e-0410-83ba-ed86b6aff4e0>
2008-06-09 17:26:13 +0000

    initial import

---
+++ gr.sch.ira.minoas.voids-ejb/trunk/ejbModule/gr/sch/ira/minoas/voids/session/BaseSearchBean.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package gr.sch.ira.minoas.voids.session;
+
+/**
+ * @author <a href="mailto:filippos@slavik.gr">Filippos Slavik</a>
+ *
+ */
+public abstract class BaseSearchBean {
+
+}
