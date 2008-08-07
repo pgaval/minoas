e4791f41d64b9e6b5e7d8ac398e8a7e30754abe1
slavikos <slavikos@c46bf295-244e-0410-83ba-ed86b6aff4e0>
2008-08-07 21:43:39 +0000

    implemented more search methods

--- gr.sch.ira.minoas-ejb/trunk/ejbModule/gr/sch/ira/minoas/core/session/CoreSearching.java
+++ gr.sch.ira.minoas-ejb/trunk/ejbModule/gr/sch/ira/minoas/core/session/CoreSearching.java
@@ -2,9 +2,12 @@ package gr.sch.ira.minoas.core.session;
 
 import gr.sch.ira.minoas.model.core.School;
 import gr.sch.ira.minoas.model.core.Specialization;
+import gr.sch.ira.minoas.model.security.Role;
+import gr.sch.ira.minoas.model.security.RoleGroup;
 import gr.sch.ira.minoas.model.voids.TeachingRequirement;
 
 import java.util.Collection;
+import java.util.List;
 
 import javax.ejb.Remote;
 
@@ -25,4 +28,12 @@ public interface CoreSearching {
 	public Collection<School> searchShools(String school_search_pattern, String regionCode);
 	
 	public Specialization getSpecialization(String id);
+	
+	public Collection<Role> searchRoles(String role_search_pattern);
+	
+	public Role findRole(String roleID);
+	
+	public RoleGroup findRoleGroup(String roleGroupID);
+	
+	public Collection<RoleGroup> searchRoleGroups(String roleGroup_search_pattern);
 }
--- gr.sch.ira.minoas-ejb/trunk/ejbModule/gr/sch/ira/minoas/core/session/CoreSearchingBean.java
+++ gr.sch.ira.minoas-ejb/trunk/ejbModule/gr/sch/ira/minoas/core/session/CoreSearchingBean.java
@@ -2,10 +2,15 @@ package gr.sch.ira.minoas.core.session;
 
 import gr.sch.ira.minoas.model.core.School;
 import gr.sch.ira.minoas.model.core.Specialization;
+import gr.sch.ira.minoas.model.security.Role;
+import gr.sch.ira.minoas.model.security.RoleGroup;
 import gr.sch.ira.minoas.model.voids.TeachingRequirement;
+import gr.sch.ira.minoas.session.BaseStatelessSeamComponentImpl;
 
 import java.util.Collection;
+import java.util.List;
 
+import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,20 +22,19 @@ import org.jboss.seam.annotations.Scope;
 @Name("coreSearching")
 @Scope(ScopeType.STATELESS)
 @Stateless
-public 
-class CoreSearchingBean implements CoreSearching {
+@Local(CoreSearching.class)
+public class CoreSearchingBean extends BaseStatelessSeamComponentImpl implements CoreSearching {
+
 	@PersistenceContext
 	private EntityManager em;
 
 	/**
 	 * @see gr.sch.ira.minoas.core.session.CoreSearching#searchVoids(gr.sch.ira.minoas.model.core.School,
-	 *      gr.sch.ira.minoas.model.core.Specialization, int)
+	 * gr.sch.ira.minoas.model.core.Specialization, int)
 	 */
-	public Collection<TeachingRequirement> searchVoids(School school,
-			Specialization specialization, int minHours) {
-		return em
-				.createQuery(
-						"SELECT v from TeachingVoid v WHERE v.school = :school ORDER BY (v.specialisation.id)")
+	@SuppressWarnings("unchecked")
+	public Collection<TeachingRequirement> searchVoids(School school, Specialization specialization, int minHours) {
+		return em.createQuery("SELECT v from TeachingVoid v WHERE v.school = :school ORDER BY (v.specialisation.id)")
 				.setParameter("school", school).getResultList();
 	}
 
@@ -43,10 +47,9 @@ class CoreSearchingBean implements CoreSearching {
 
 	/**
 	 * @see gr.sch.ira.minoas.core.session.CoreSearching#searchShools(java.lang.String,
-	 *      java.lang.String)
+	 * java.lang.String)
 	 */
-	public Collection<School> searchShools(String school_search_pattern,
-			String regionCode) {
+	public Collection<School> searchShools(String school_search_pattern, String regionCode) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -54,10 +57,10 @@ class CoreSearchingBean implements CoreSearching {
 	/**
 	 * @see gr.sch.ira.minoas.core.session.CoreSearching#searchShools(java.lang.String)
 	 */
+	@SuppressWarnings("unchecked")
 	public Collection<School> searchShools(String school_search_pattern) {
-		return em
-				.createQuery(
-						"SELECT s from School s WHERE lower(s.title) LIKE :search_pattern AND s.ministryCode != '0000000'")
+		return em.createQuery(
+				"SELECT s from School s WHERE lower(s.title) LIKE :search_pattern AND s.ministryCode != '0000000'")
 				.setParameter("search_pattern", school_search_pattern).getResultList();
 	}
 
@@ -68,6 +71,47 @@ class CoreSearchingBean implements CoreSearching {
 		return em.find(Specialization.class, id);
 	}
 
+	@SuppressWarnings("unchecked")
+	public Collection<Role> searchRoles(String role_search_pattern) {
+		String pattern = getSearchPattern(role_search_pattern); 
+		info("searching for roles with #0 search pattern", pattern);
+		Collection return_value = em.createQuery("SELECT r from Role r WHERE lower(r.id) LIKE :search_pattern")
+				.setParameter("search_pattern", pattern).getResultList();
+		info("found totally #0 role(s).", return_value.size());
+		return return_value;
+	}
+
+	/**
+	 * @see gr.sch.ira.minoas.core.session.CoreSearching#findRole(java.lang.String)
+	 */
+	public Role findRole(String roleID) {
+		return em.find(Role.class, roleID);
+	}
+
+	/**
+	 * @see gr.sch.ira.minoas.core.session.CoreSearching#findRoleGroup(java.lang.String)
+	 */
+	public RoleGroup findRoleGroup(String roleGroupID) {
+		return em.find(RoleGroup.class, roleGroupID);
+	}
+
+	/**
+	 * @see gr.sch.ira.minoas.core.session.CoreSearching#searchRoleGroups(java.lang.String)
+	 */
+	@SuppressWarnings("unchecked")
+	public Collection<RoleGroup> searchRoleGroups(String roleGroup_search_pattern) {
+		String pattern = getSearchPattern(roleGroup_search_pattern); 
+		info("searching for role groups with #0 search pattern", pattern);
+		Collection return_value = em.createQuery("SELECT r from RoleGroup r WHERE lower(r.id) LIKE :search_pattern")
+				.setParameter("search_pattern", pattern).getResultList();
+		info("found totally #0 role group(s).", return_value.size());
+		return return_value;
+	}
+	
+	protected String getSearchPattern(String searchString) {
+		return searchString == null ? "%" : '%' + searchString
+				.toLowerCase().replace('*', '%') + '%';
+	}
 	
 
 }
