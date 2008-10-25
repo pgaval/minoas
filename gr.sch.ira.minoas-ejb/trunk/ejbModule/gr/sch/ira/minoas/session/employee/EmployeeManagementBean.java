63fa9e2d13f9f6dc0cb19c5b3903d0973cbceed2
slavikos <slavikos@c46bf295-244e-0410-83ba-ed86b6aff4e0>
2008-10-25 21:16:58 +0000

    working on secondments

--- gr.sch.ira.minoas-ejb/trunk/ejbModule/gr/sch/ira/minoas/session/employee/EmployeeManagementBean.java
+++ gr.sch.ira.minoas-ejb/trunk/ejbModule/gr/sch/ira/minoas/session/employee/EmployeeManagementBean.java
@@ -3,16 +3,16 @@
  */
 package gr.sch.ira.minoas.session.employee;
 
-import gr.sch.ira.minoas.model.core.SchoolYear;
+import gr.sch.ira.minoas.model.employee.DeputyEmployee;
 import gr.sch.ira.minoas.model.employee.Employee;
+import gr.sch.ira.minoas.model.employee.RegularEmployee;
 import gr.sch.ira.minoas.model.employement.Employment;
 import gr.sch.ira.minoas.model.employement.Secondment;
 import gr.sch.ira.minoas.model.employement.SecondmentType;
-import gr.sch.ira.minoas.seam.components.BaseStatefulSeamComponentImpl;
 import gr.sch.ira.minoas.seam.components.CoreSearching;
+import gr.sch.ira.minoas.seam.components.EmployeeUtil;
 import gr.sch.ira.minoas.seam.components.IBaseStatefulSeamComponent;
 
-import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
@@ -21,22 +21,13 @@ import javax.ejb.Local;
 import javax.ejb.Remove;
 import javax.ejb.Stateful;
 import javax.faces.application.FacesMessage;
-import javax.faces.application.FacesMessage.Severity;
-import javax.persistence.EntityManager;
 
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Begin;
 import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.Destroy;
 import org.jboss.seam.annotations.End;
-import org.jboss.seam.annotations.Factory;
-import org.jboss.seam.annotations.FlushModeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Out;
-import org.jboss.seam.annotations.TransactionPropagationType;
-import org.jboss.seam.annotations.Transactional;
-import org.jboss.seam.annotations.Unwrap;
 import org.jboss.seam.annotations.security.Restrict;
 
 /**
@@ -47,8 +38,7 @@ import org.jboss.seam.annotations.security.Restrict;
 @Stateful
 @Restrict("#{identity.loggedIn}")
 @Local( { IBaseStatefulSeamComponent.class, IEmployeeManagement.class, IEmployeeAware.class })
-public class EmployeeManagementBean extends EmployeeAwareSeamComponent
-		implements IEmployeeManagement {
+public class EmployeeManagementBean extends EmployeeAwareSeamComponent implements IEmployeeManagement {
 
 	/**
 	 * 
@@ -76,71 +66,6 @@ public class EmployeeManagementBean extends EmployeeAwareSeamComponent
 		super.create();
 	}
 
-	public String prepareNewSecondmet() {
-		/*
-		 * check if the employee has already an active secondment
-		 */
-		setEmployeeActiveSecondment(coreSearching
-				.getEmployeeActiveSecondment(getActiveEmployee()));
-		if (getEmployeeActiveSecondment() != null) {
-			warn("employee '#0' has already an active secondment '#1'.",
-					getActiveEmployee(), getEmployeeActiveSecondment());
-		}
-		/* prepare the new secondment object */
-
-		/* fill the obvious values */
-		newSecondment = new Secondment();
-		newSecondment.setEmployeeRequested(false);
-		newSecondment.setActive(Boolean.TRUE);
-		newSecondment.setSupersededBy(null);
-		newSecondment.setSchoolYear(getActiveSchoolYear());
-		newSecondment.setEmployeeRequested(Boolean.TRUE);
-		newSecondment.setEmployee(getActiveEmployee());
-		newSecondment.setEstablished(new Date((Calendar
-				.getInstance(greekLocale)).getTimeInMillis()));
-		Calendar dueTo = Calendar.getInstance(greekLocale);
-		dueTo.set(Calendar.MONTH, Calendar.JUNE);
-		dueTo.set(Calendar.DAY_OF_MONTH, 30);
-		newSecondment.setDueTo(new Date(dueTo.getTimeInMillis()));
-
-		/* find the employee's current employment */
-		Collection<Employment> activeEmployments = coreSearching
-				.getEmployeeActiveEmployments(getActiveEmployee());
-		if (activeEmployments.size() != 1) {
-			/*
-			 * for a secondment to be valid, the employee in question needs to
-			 * have exactly one active employment.
-			 */
-			warn(
-					"new secondment for employee '#0' with ID '#1' is not possible, since the employee in question needs to have exactly one active employment and not #2 employment(s)",
-					getActiveEmployee(), getActiveEmployee().getId(),
-					activeEmployments.size());
-
-			getFacesMessages()
-					.addFromResourceBundle(
-							FacesMessage.SEVERITY_ERROR,
-							"EmployeeManagementBean.ONE_EMPLOYMENT_NEEDED_FOR_SECONDMENT",
-							getActiveEmployee());
-			return FAILURE_OUTCOME;
-		}
-		Employment affected_employment = activeEmployments.iterator().next();
-		
-		newSecondment.setAffectedEmployment(affected_employment);
-		newSecondment
-				.setSourcePYSDE(getMinoasDatabase().merge(affected_employment.getSchool().getPysde()));
-		newSecondment.setSourceUnit(getMinoasDatabase().merge(affected_employment.getSchool()));
-
-		/*
-		 * adjust working hours by retrieving the mandatory working hours from
-		 * the affected employment. When copying do not copy any decrement
-		 * since, this can be done later on.
-		 */
-		newSecondment.setFinalWorkingHours(affected_employment
-				.getMandatoryWorkingHours());
-
-		return SUCCESS_OUTCOME;
-	}
-
 	/**
 	 * @see gr.sch.ira.minoas.seam.components.BaseStatefulSeamComponentImpl#destroy()
 	 */
@@ -158,28 +83,84 @@ public class EmployeeManagementBean extends EmployeeAwareSeamComponent
 		return employeeActiveSecondment;
 	}
 
+	public String prepareNewSecondmet() {
+		Employee employee = getMinoasDatabase().merge(getActiveEmployee());
+		/*
+		 * A secondment for the given employee can be created only if the employ
+		 * is regular or deputy (very rare)
+		 */
+		if (!(employee instanceof RegularEmployee || employee instanceof DeputyEmployee)) {
+			// oops! the employee is not valid for secondment
+			warn("active employee '#0' is not valid for secondment (regular or deputy)", employee);
+			getFacesMessages().addFromResourceBundle(FacesMessage.SEVERITY_ERROR,
+					"EmployeeManagementBean.EMPLOYEE_TYPE_NOT_VALID_FOR_SECONDMENT", getActiveEmployee());
+			return FAILURE_OUTCOME;
+		}
+		/*
+		 * check if the employee has already an active secondment
+		 */
+		setEmployeeActiveSecondment(coreSearching.getEmployeeActiveSecondment(employee));
+		if (getEmployeeActiveSecondment() != null) {
+			warn("employee '#0' has already an active secondment '#1'.", getActiveEmployee(),
+					getEmployeeActiveSecondment());
+		}
+		/* prepare the new secondment object */
+
+		/* fill the obvious values */
+		newSecondment = new Secondment();
+		newSecondment.setEmployeeRequested(false);
+		newSecondment.setActive(Boolean.TRUE);
+		newSecondment.setSupersededBy(null);
+		newSecondment.setSchoolYear(getActiveSchoolYear());
+		newSecondment.setEmployeeRequested(Boolean.TRUE);
+		newSecondment.setEmployee(getActiveEmployee());
+		newSecondment.setEstablished(new Date((Calendar.getInstance(greekLocale)).getTimeInMillis()));
+		Calendar dueTo = Calendar.getInstance(greekLocale);
+		dueTo.set(Calendar.MONTH, Calendar.JUNE);
+		dueTo.set(Calendar.DAY_OF_MONTH, 30);
+		newSecondment.setDueTo(new Date(dueTo.getTimeInMillis()));
+
+		/*
+		 * check if the employee has a current employment. Not all employees
+		 * have a current employment, especially the ones that came from other
+		 * regions.
+		 */
+
+		Employment current_employment = employee.getCurrentEmployment();
+		if (current_employment != null) {
+			newSecondment.setAffectedEmployment(current_employment);
+			newSecondment.setSourcePYSDE(getMinoasDatabase().merge(current_employment.getSchool().getPysde()));
+			newSecondment.setSourceUnit(getMinoasDatabase().merge(current_employment.getSchool()));
+			/*
+			 * adjust working hours by retrieving the mandatory working hours
+			 * from the affected employment. When copying do not copy any
+			 * decrement since, this can be done later on.
+			 */
+			newSecondment.setFinalWorkingHours(current_employment.getMandatoryWorkingHours());
+		}
+		return SUCCESS_OUTCOME;
+	}
+
 	/**
 	 * @see gr.sch.ira.minoas.session.employee.IEmployeeManagement#saveSecondment()
 	 */
 	@End
 	public String saveSecondment() {
-		/* check */
-		info("trying to save secondment #0", this.newSecondment);
+		Employee employee = getMinoasDatabase().merge(getActiveEmployee());
+		info("trying to save new seconment '#0' for employee '#1' during school year '#2'.", newSecondment, employee, getActiveSchoolYear());
+		
 		if (getEmployeeActiveSecondment() != null) {
-			Secondment activeSecondment = getEmployeeActiveSecondment();
+			Secondment activeSecondment = getMinoasDatabase().merge(getEmployeeActiveSecondment());
 			activeSecondment.setActive(Boolean.FALSE);
 			activeSecondment.setSupersededBy(newSecondment);
-			getMinoasDatabase().merge(activeSecondment);
 		}
 		newSecondment.setInsertedOn(new Date(System.currentTimeMillis()));
 		if (newSecondment.getTargetUnit() != null) {
-			newSecondment.setTargetPYSDE(newSecondment.getTargetUnit()
-					.getPysde());
+			newSecondment.setTargetPYSDE(newSecondment.getTargetUnit().getPysde());
 		}
 		setEmployeeActiveSecondment(newSecondment);
 		getMinoasDatabase().persist(newSecondment);
-		Employment employment = getMinoasDatabase().merge(
-				newSecondment.getAffectedEmployment());
+		Employment employment = newSecondment.getAffectedEmployment();
 		employment.setSecondment(newSecondment);
 		getMinoasDatabase().flush();
 		return SUCCESS_OUTCOME;
@@ -191,10 +172,8 @@ public class EmployeeManagementBean extends EmployeeAwareSeamComponent
 		return SUCCESS_OUTCOME;
 	}
 
-
 	/**
-	 * @param employeeActiveSecondment
-	 *            the employeeActiveSecondment to set
+	 * @param employeeActiveSecondment the employeeActiveSecondment to set
 	 */
 	public void setEmployeeActiveSecondment(Secondment employeeActiveSecondment) {
 		this.employeeActiveSecondment = employeeActiveSecondment;
